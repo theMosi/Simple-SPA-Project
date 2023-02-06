ff7d1f3497d24602d18b992681d3acb48fd6fd20
Mosi <mostafawshe@gmail.com>
2023-02-06 18:43:38 +0330

    add user's infos

---
+++ src/components/users/CardView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="card">
+    <div class="card-header">{{ user.name }}</div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">username: {{ user.username }}</li>
+      <li class="list-group-item">Email: {{ user.email }}</li>
+      <li class="list-group-item">Phone: {{ user.phone }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: Object,
+  },
+};
+</script>
+
+<style>
+</style>
--- src/pages/users/UsersComp.vue
+++ src/pages/users/UsersComp.vue
@@ -1,17 +1,35 @@
 <template>
-  <h1>Users</h1>
+  <div class="container mt-5">
+    <div class="row g-3">
+      <div v-if="loading" class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+      <div v-else class="col-md-4" v-for="user in users" :key="user.id">
+        <UserCardView :user="user" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
+import { ref } from "vue";
+import UserCardView from "@/components/users/CardView.vue";
 
 export default {
+  components: {
+    UserCardView,
+  },
   setup() {
+    const users = ref([]);
+    const loading = ref(true);
+
     function getUsers() {
       axios
         .get("https://jsonplaceholder.typicode.com/users")
         .then(function (response) {
-          console.log(response.data);
+          users.value = response.data;
+          loading.value = false;
         })
         .catch(function (error) {
           console.log(error);
@@ -19,6 +37,8 @@ export default {
     }
 
     getUsers();
+
+    return { users, loading };
   },
 };
 </script>
