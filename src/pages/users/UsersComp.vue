f90ba334540284d24be1226b9dfccf354bf31bbf
Mosi <mostafawshe@gmail.com>
2023-02-06 17:51:43 +0330

    add axios

---
+++ src/pages/users/UsersComp.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Users</h1>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  setup() {
+    function getUsers() {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then(function (response) {
+          console.log(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+
+    getUsers();
+  },
+};
+</script>
+
+<style>
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeComp from './pages/HomeComp.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeComp from './pages/HomeComp.vue'
+import IndexUsers from './pages/users/UsersComp.vue'
 
 const routes = [
-    { path: "/", name: "home", component: HomeComp }
+    { path: "/", name: "home", component: HomeComp },
+    { path: "/users", name: "users", component: IndexUsers }
 ];
 
 const router = createRouter({
